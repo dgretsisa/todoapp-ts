cdefb9303637af99dfabba2130b3f4e43bd60c5c
Piso Gaming <pisogaming@gmail.com>
2021-05-18 19:29:21 +0800

    Refactoring code files

--- src/app/interfaces/ListInterface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-import { TodoInterface } from './TodoInterface';
-
-export interface ListInterface extends Document {
-    title: string;
-    author: string;
-    todos: TodoInterface[]
-}
--- src/app/interfaces/TodoInterface.ts
+++
@@ -1,8 +0,0 @@
-import { Document, Schema } from 'mongoose';
-import { ListInterface } from './ListInterface';
-
-export interface TodoInterface extends Document {
-    list: ListInterface['_id'];
-    text: string,
-    status: boolean;
-}
